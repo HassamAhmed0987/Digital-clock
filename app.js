37c30bac13c4c544a91000e87226900db5a0382e
HassamAhmed0987 <hassamansari0987@gmail.com>
2026-03-07 06:20:55 +0500

    final

---
+++ app.js
@@ -0,0 +1,48 @@
+
+const hours = document.querySelector(".hours")
+const minutes = document.querySelector(".minutes")
+const second = document.querySelector(".second")
+const date = document.querySelector(".date")
+
+
+
+
+setInterval(function () {
+    let nowTime = new Date()
+
+    let h = nowTime.getHours()
+    let m = nowTime.getMinutes()
+    let s = nowTime.getSeconds()
+    let time = `${h}:${m}:${s}`
+    date.textContent = nowTime.toDateString()
+    hours.textContent = h
+    // minutes.textContent = m
+    // second.textContent = `0${s}`
+    if(s >= 10 && s <= 60){
+        second.textContent = `${s}` 
+    }else (
+        second.textContent = `0${s}`
+    )
+    if(m >= 10 && m <= 60){
+        minutes.textContent = `${m}` 
+    }else (
+        minutes.textContent = `0${m}`
+    )
+    if(h >= 10 && m <= 24){
+        hours.textContent = `${h}` 
+    }else (
+        hours.textContent = `0${h}`
+    )
+    // console.log(nowTime.getDay())
+
+})
+
+
+
+
+
+
+
+
+
+
